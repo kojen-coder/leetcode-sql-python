8e038fb2c226ae7c3492771a3ee4183a3b4d0017
karenwang <kojenwang7@gmail.com>
2023-09-20 00:24:00 -0400

    ifnull(...,0), round(..., 2)

---
+++ 597. Friend Request I: Overall Acceptance Rate.sql
@@ -0,0 +1,1 @@
+select round(ifnull((select count(distinct requester_id, accepter_id) from requestaccepted) / (select count(distinct sender_id, send_to_id) from friendrequest), 0), 2) as accept_rate
