93e06f15967bb24c18299024dd2835d1e9f4fc17
karenwang <kojenwang7@gmail.com>
2023-09-28 23:54:18 -0400

    avg(case when...), subquery

---
+++ 1211. Queries Quality and Percentage.sql
@@ -0,0 +1,6 @@
+SELECT a.query_name, round(avg(a.quality),2) as quality,
+round(avg(CASE WHEN a.rating <3 THEN 1 ELSE 0 END) * 100, 2) as poor_query_percentage
+FROM
+(SELECT query_name, position, rating, rating/position as quality
+FROM Queries) a
+GROUP BY a.query_name
