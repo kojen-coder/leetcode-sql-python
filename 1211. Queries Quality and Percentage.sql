a7950c9a2a6927b9e52bc1d7f4505dd7435d903c
karenwang <kojenwang7@gmail.com>
2023-09-29 00:23:19 -0400

    avg(case when)

--- 1211. Queries Quality and Percentage.sql
+++ 1211. Queries Quality and Percentage.sql
@@ -1,3 +1,5 @@
+# Write your MySQL query statement below
+
 SELECT a.query_name, round(avg(a.quality),2) as quality,
 round(avg(CASE WHEN a.rating <3 THEN 1 ELSE 0 END) * 100, 2) as poor_query_percentage
 FROM
