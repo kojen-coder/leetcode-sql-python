81f984b395727dd4461285b4cccd61af364d4e83
karenwang <kojenwang7@gmail.com>
2024-02-19 23:29:34 -0500

    not in ()

---
+++ 2394. Emplloyees With Deductions.sql
@@ -0,0 +1,9 @@
+with temp as(
+    select e.employee_id, e.needed_hours, sum(ceil(timestampdiff(minute, in_time, out_time))) as total_hr
+    from employees e, logs l where e.employee_id = l.employee_id
+    group by e.employee_id, e.needed_hours
+)
+
+select employee_id from employees
+where employee_id not in (select employee_id from temp where (total_hr)/60 >= needed_hours)
+order by employee_id
