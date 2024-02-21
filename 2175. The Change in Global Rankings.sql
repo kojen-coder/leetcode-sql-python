e3d197352bfe7dd9a9e9ed6e98b39dc1988ff7a4
karenwang <kojenwang7@gmail.com>
2024-02-20 20:19:29 -0500

    Window Function

---
+++ 2175. The Change in Global Rankings.sql
@@ -0,0 +1,11 @@
+with tmp as (
+    SELECT team_id, sum(points_change) as points_change
+    FROM PointsChange GROUP BY team_id
+)
+
+SELECT a.team_id, a.name, CAST(a.rank1 AS SIGNED) - CAST(a.rank2 AS SIGNED) as rank_diff
+FROM
+(SELECT t.team_id, t.name, t.points, t.points+t1.points_change  as new_point,
+rank() over(order by t.points desc, T.name) as rank1,
+row_number() over(order by t.points+t1.points_change desc, T.name) as rank2
+FROM TeamPoints t LEFT JOIN tmp t1 on t.team_id=t1.team_id) a
