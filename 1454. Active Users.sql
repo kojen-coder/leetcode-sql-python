e76a06bc78dd0fba42b6320ae24d992e4a2b2f0a
karenwang <kojenwang7@gmail.com>
2024-03-14 01:32:25 -0400

    adjust comments

--- 1454. Active Users.sql
+++ 1454. Active Users.sql
@@ -6,7 +6,7 @@ from(
     datediff(l.login_date,
     lag(l.login_date, 4) over(partition by l.id order by l.login_date)) as lag4
     from(
-        select distinct id, login_date from logins # Because there might be 2 same dates
+        select distinct id, login_date from logins -- Because there might be 2 same dates
     ) l inner join accounts a
     on a.id=l.id
 )c
