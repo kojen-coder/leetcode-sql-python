681c9eb7f8f2ad57692d4454cff5dd528165568b
karenwang <kojenwang7@gmail.com>
2024-07-29 23:25:12 +0800

    Recursion to solve this problem

--- DataLemur/Factorial Formula [Microsoft Python Interview Question].py
+++ DataLemur/Factorial Formula [Microsoft Python Interview Question].py
@@ -10,3 +10,10 @@ def factorial(n):
             i += 1
 
     return result
+
+# Preferred Solution: Using recursion to solve this problem
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n*factorial(n-1)
