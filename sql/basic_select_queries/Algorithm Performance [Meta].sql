87a7ea7c06a1666891341904999b7d97770bd758
karenwang <kojenwang7@gmail.com>
2024-10-22 18:04:29 -0400

    Find the highest rating for the search id - we can use case when to assign the rating and use group by and max to get the maximum rating for each search id

---
+++ sql/basic_select_queries/Algorithm Performance [Meta].sql
@@ -0,0 +1,11 @@
+select
+search_id,
+max(case
+    when clicked = 0 then 1
+    when clicked != 0 and search_results_position > 3 then 2
+    when clicked != 0 and search_results_position <= 3 then 3
+    end) as max_rating
+from fb_search_events
+group by search_id
+
+;
